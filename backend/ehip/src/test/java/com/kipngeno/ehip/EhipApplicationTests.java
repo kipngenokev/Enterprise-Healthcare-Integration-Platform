eb7f26e40bc8a346bd66c40f4c1ebaa64ea0c534
kipngenokev <kkipngeno409@gmail.com>
2026-07-18 03:26:31 +0300

    build:initialize Spring Boot project

---
+++ backend/ehip/src/test/java/com/kipngeno/ehip/EhipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kipngeno.ehip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EhipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
